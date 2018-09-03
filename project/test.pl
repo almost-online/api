698e84f189f78398fd00fec40bb05df36a8d8ff4
star <almost@online.com.ua>
2018-09-03 23:54:33 +0300

    add test pl

--- project/test.pl
+++ project/test.pl
@@ -1,29 +1,4 @@
-BEGIN{
-use File::Basename;
-use File::Spec;
-$perl_dir = File::Spec->rel2abs(dirname(__FILE__));
-$perl_dir = $perl_dir . "/../perl";
-push @INC, $perl_dir;
-}
-
-# Import Citrusleaf
-use citrusleaf;
-use perl_citrusleaf;
-
-citrusleaf::citrusleaf_init();
-
-asc = citrusleaf::citrusleaf_cluster_create();
-
-return_value = citrusleaf::citrusleaf_cluster_add_host($asc, "127.0.0.1", 3000, 1000);
-
-return_value = citrusleaf::citrusleaf_put($asc, "test", "myset",$key_obj, $bins, 2, $cl_wp);
-
-size = citrusleaf::new_intp();
-generation = citrusleaf::new_intp();
-
-
-citrusleaf::citrusleaf_free_bins($bins, $number_bins, $bins_get_all);
-citrusleaf::delete_intp($size);
-citrusleaf::delete_intp($generation);
-citrusleaf::delete_cl_bin_p($bins_get_all);
+#!/usr/bin/perl -wT
+print "Content-type: text/plain\n\n";
+print "This is the TExt file";
 
